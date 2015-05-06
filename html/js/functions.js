271abc515bb3ba2b6bad827b65752e1ffc460142
Jeff <jheppert@gmail.com>
2015-05-05 22:01:26 -0700

    icon buttons working

--- html/js/functions.js
+++ html/js/functions.js
@@ -14,11 +14,17 @@ function returnPageLinks(currentPageID){
         return "<strong>THE END</strong>";
     } else {
         var resultLength = storyDatabase.pages[currentPageID].choiceLinks.length;
-        var resultString = "";
+        var resultString = "<div class='bigLinkContainer'>";
+        for(var i=0;i<resultLength; i++){
+            resultString += "<a class='iconLink iconLink-" + resultLength + "' data-linkMarco=" + i + " href='javascript:linkMarcoPolo(" + i + ")'><img class='icon' src='img/" + storyDatabase.pages[currentPageID].choiceLinks[i].linkIcon +".png' /></a>"
+        }
+        resultString += "<div class='pageLinkContainer'>";
         for(var i=0;i<resultLength; i++){
             // PUT STUFF HERE FOR ICON CHOOSING MENU
-            resultString += "<a class='pageLink' data=linkPolo=" + i + " href='javascript:addNewSection(" + storyDatabase.pages[currentPageID].choiceLinks[i].linkDestination + ");'>" + storyDatabase.pages[currentPageID].choiceLinks[i].linkText + "</a> ";
+            resultString += "<a class='pageLink' data-linkPolo=" + i + " href='javascript:addNewSection(" + storyDatabase.pages[currentPageID].choiceLinks[i].linkDestination + ");'>" + storyDatabase.pages[currentPageID].choiceLinks[i].linkText + "</a> ";
         }
+        resultString += "</div>";
+        resultString += "</div>";
         return resultString;
     }
 }
@@ -30,9 +36,8 @@ function linkMarcoPolo(linkMarco) {
     // hide all links (remove class?)
     // show the data-polo matched number link (add class?)
     // make sure to use sibling to only affect current group links
-    // alert(linkMarco);
-    $(this).siblings("div.pageLinkContainer").addClass("pageLinkActive");
-    alert("hi");
+    $("a").removeClass("pageLinkActive");
+    $(".pageLinkContainer").children("a[data-linkPolo='" + linkMarco +"']").addClass("pageLinkActive");
 }
 
 function returnPageSetting(currentPageID){
@@ -43,7 +48,7 @@ function returnPageSetting(currentPageID){
 // Constructs a new section element based on the ID passed in
 // Appends the new section as the last element in the #content
 function addNewSection(newSectionID){
-    $("a.pageLink").fadeOut();
+    $(".bigLinkContainer").fadeOut();
     $("section#activeSection").removeAttr("id");
     var newSection = "<section class='page page-" + newSectionID + " " + returnPageSetting(newSectionID) + "' id='activeSection'><div class='dialog'><div><p>" + returnPageText(newSectionID) + "</p></div><div>" + returnPageLinks(newSectionID) + "</div></div></section>";
     $("#content").append(newSection);
